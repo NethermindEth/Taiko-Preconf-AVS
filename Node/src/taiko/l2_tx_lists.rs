f49e14e00b103ae4d35b971e27e2c39797eb717f
Maciej Skrzypkowski <mskr@gmx.com>
2024-07-16 17:16:11 +0200

    moved tx list response to the the separate file

--- Node/src/taiko/l2_tx_lists.rs
+++ Node/src/taiko/l2_tx_lists.rs
@@ -53,52 +53,10 @@ mod tests {
 
     #[test]
     fn test_decompose_pending_lists_json() {
-        let json_data = serde_json::json!(
-            {
-                "TxLists": [
-                    [
-                        {
-                            "type": "0x0",
-                            "chainId": "0x28c61",
-                            "nonce": "0x8836",
-                            "to": "0x8b14d287b4150ff22ac73df8be720e933f659abc",
-                            "gas": "0x35f30",
-                            "gasPrice": "0xf4b87001",
-                            "maxPriorityFeePerGas": null,
-                            "maxFeePerGas": null,
-                            "value": "0x0",
-                            "input": "0x3161b7f60000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000027e9000000000000000000000000000000000000000000000005751b6fc9babade290000000000000000000000000000000000000000000000000000000008f811330000000000000000000000000000000000000000000000000000000000000010",
-                            "v": "0x518e5",
-                            "r": "0xb7b4b5540e08775ebb3c077ca7d572378cdb6ed55e3387173f8248578cc073e9",
-                            "s": "0x1f8860f90b61202d4070d1eba494d38c2cb02c749ea1ec542c8d02e5ceeb6439",
-                            "hash": "0xc653e446eafe51eea1f46e6e351adbd1cc8a3271e6935f1441f613a58d441f6a"
-                        },
-                        {
-                            "type": "0x2",
-                            "chainId": "0x28c61",
-                            "nonce": "0x26d0",
-                            "to": "0x2f6ef5baae08ae9df23528c217a77f03f93b690e",
-                            "gas": "0x1a09b",
-                            "gasPrice": null,
-                            "maxPriorityFeePerGas": "0xcbef0801",
-                            "maxFeePerGas": "0xcbef0801",
-                            "value": "0x0",
-                            "input": "0xbb2cd728000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000002000000000000000000000000ae2c46ddb314b9ba743c6dee4878f151881333d90000000000000000000000007d16e966c879ed6ad9972ddd5376c41a427d2c2a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000004cd712049c0000000000000000000000000000000000000000000000000000057b3ac2af84",
-                            "accessList": [],
-                            "v": "0x0",
-                            "r": "0xbfc407896ee6ed36962d1a591b41aa9a17fe19e176e61cb54a1cd69e3a337508",
-                            "s": "0x621b2448dda4d797447935c051e3908f65dfb3729415280a04cb02c4474baefe",
-                            "yParity": "0x0",
-                            "hash": "0xffbcd2fab90f1bf314ca2da1bf83eeab3d17fd58a0393d29a697b2ff05d0e65c"
-                        }
-                    ]
-                ],
-                "TxListBytes": [
-                    "eJz6ybTuJ6NeU4dZy5cdBYzNzPEGU7pFLrVvEeX/pHXc9se+Ir7J9qmz9jTsOGKYuP0bA36gQECeEb+0+kscEqyl0vknd+26p4lPN8cPQWMC9gs0s0o8XbB9y9YQPo7yuN027DXLrxaZ99zOuxpn3C5u3+QR3nOg+OUC+Y6En9yJCroOBRdfL5lyuUdng07JvIVvQnR6mZ6ee51iuZOxiuknY1kzU09ik9qFltPvORjBRDPjgtlT9PO+7lrHsW7uJ1ONQ+LL65l/WmfyNexkZNmtc12DgPMcCMgvICDPhMxZp+N2d7PIzl0lNrnvPCo+BnYIG99Elq8Ve5l2ovJt1s3puneDy45IOdXqaJFiPhrwuS7EMge3NGu11aH1LQcaFuw/wt6Z9+yt2TRdqUhpx1WzxP9JPix7JrPVS+baPCvjUo4FSdIqHneXXJ/uUml6IPDxhP7U+5uLpohqcLGcZjri7r3uHyAAAP//huiQHQ=="
-                ],
-                "ParentMetaHash": "0x2bcf3b1bb0c4066aa46ba6e99b79d7602f124d5ae8fcffd2977b1c2138aa61bc"
-            }
-        );
+        let json_data = serde_json::from_str(
+            include_str!("../utils/tx_lists_test_response.json")
+        )
+        .unwrap();
 
         let result = decompose_pending_lists_json(json_data).unwrap();
 
