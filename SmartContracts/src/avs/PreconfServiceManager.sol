4c92063d5facb2b9d257eba980dfb143f13b8357
AnshuJalan <anshujalan206@gmail.com>
2024-08-27 14:38:36 +0530

    feat: remove eigenlayer middleware calls

--- SmartContracts/src/avs/PreconfServiceManager.sol
+++ SmartContracts/src/avs/PreconfServiceManager.sol
@@ -4,47 +4,66 @@ pragma solidity 0.8.25;
 import {IPreconfServiceManager} from "../interfaces/IPreconfServiceManager.sol";
 import {IPreconfTaskManager} from "../interfaces/IPreconfTaskManager.sol";
 import {ISlasher} from "../interfaces/eigenlayer-mvp/ISlasher.sol";
-import {
-    IAVSDirectory,
-    IRegistryCoordinator,
-    IRewardsCoordinator,
-    IStakeRegistry,
-    ServiceManagerBase
-} from "eigenlayer-middleware/ServiceManagerBase.sol";
+import {IAVSDirectory} from "../interfaces/eigenlayer-mvp/IAVSDirectory.sol";
 
-contract PreconfServiceManager is ServiceManagerBase, IPreconfServiceManager {
+contract PreconfServiceManager is IPreconfServiceManager {
+    address internal immutable preconfRegistry;
+    IAVSDirectory internal immutable avsDirectory;
     IPreconfTaskManager internal immutable preconfTaskManager;
     ISlasher internal immutable slasher;
 
     mapping(address operator => uint256 timestamp) public stakeLockedUntil;
 
     constructor(
+        address _preconfRegistry,
         IAVSDirectory _avsDirectory,
-        IRewardsCoordinator _rewardsCoordinator,
-        IRegistryCoordinator _registryCoordinator,
-        IStakeRegistry _stakeRegistry,
         IPreconfTaskManager _taskManager,
         ISlasher _slasher
-    ) ServiceManagerBase(_avsDirectory, _rewardsCoordinator, _registryCoordinator, _stakeRegistry) {
+    ) {
+        _preconfRegistry;
+        avsDirectory = _avsDirectory;
         preconfTaskManager = _taskManager;
         slasher = _slasher;
     }
 
     modifier onlyPreconfTaskManager() {
         if (msg.sender != address(preconfTaskManager)) {
-            revert SenderIsNotPreconfTaskManager(msg.sender);
+            revert SenderIsNotPreconfTaskManager();
         }
         _;
     }
 
+    modifier onlyPreconfRegistry() {
+        if (msg.sender != preconfRegistry) {
+            revert SenderIsNotPreconfRegistry();
+        }
+        _;
+    }
+
+    /// @dev Simply relays the call to the AVS directory
+    function registerOperatorToAVS(address operator, IAVSDirectory.SignatureWithSaltAndExpiry memory operatorSignature)
+        external
+        onlyPreconfRegistry
+    {
+        avsDirectory.registerOperatorToAVS(operator, operatorSignature);
+    }
+
+    /// @dev Simply relays the call to the AVS directory
+    function deregisterOperatorFromAVS(address operator) external onlyPreconfRegistry {
+        avsDirectory.deregisterOperatorFromAVS(operator);
+    }
+
+    /// @dev This not completely functional until Eigenlayer decides the logic of their Slasher.
+    ///  for now this simply sets a value in the storage and releases an event.
     function lockStakeUntil(address operator, uint256 timestamp) external onlyPreconfTaskManager {
         stakeLockedUntil[operator] = timestamp;
         emit StakeLockedUntil(operator, timestamp);
     }
 
+    /// @dev This not completely functional until Eigenlayer decides the logic of their Slasher.
     function slashOperator(address operator) external onlyPreconfTaskManager {
         if (slasher.isOperatorSlashed(operator)) {
-            revert OperatorAlreadySlashed(operator);
+            revert OperatorAlreadySlashed();
         }
         slasher.slashOperator(operator);
     }
--- SmartContracts/src/interfaces/IPreconfServiceManager.sol
+++ SmartContracts/src/interfaces/IPreconfServiceManager.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-import {ServiceManagerBase} from "eigenlayer-middleware/ServiceManagerBase.sol";
+import {IAVSDirectory} from "./eigenlayer-mvp/IAVSDirectory.sol";
 
 interface IPreconfServiceManager {
     event StakeLockedUntil(address indexed operator, uint256 timestamp);
 
-    error SenderIsNotPreconfTaskManager(address);
-    error OperatorAlreadySlashed(address);
+    /// @dev Only callable by the task manager
+    error SenderIsNotPreconfTaskManager();
+    /// @dev Only callable by the registry
+    error SenderIsNotPreconfRegistry();
+    /// @dev The operator is already slashed
+    error OperatorAlreadySlashed();
+
+    /// @dev Only callable by the registry
+    function registerOperatorToAVS(address operator, IAVSDirectory.SignatureWithSaltAndExpiry memory operatorSignature)
+        external;
+
+    /// @dev Only callable by the registry
+    function deregisterOperatorFromAVS(address operator) external;
 
     /// @dev Called by PreconfTaskManager to prevent withdrawals of stake during preconf or lookahead dispute period
     function lockStakeUntil(address operator, uint256 timestamp) external;
