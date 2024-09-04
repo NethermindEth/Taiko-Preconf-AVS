948ea8e3ca04ee1e5586cbc0798869d1b0bf073a
Maciej Skrzypkowski <mskr@gmx.com>
2024-09-04 13:00:40 +0200

    Usage of proper lookahead for proposer

--- Node/src/utils/types.rs
+++ Node/src/utils/types.rs
@@ -1,6 +1,10 @@
-pub type PreconferAddress = [u8; 20];
 pub type ECDSASignature = [u8; 65]; // ECDSA 65 bytes signature
 pub type BLSCompressedPublicKey = [u8; 48];
+
+pub type PreconferAddress = [u8; 20];
+pub const PRECONFER_ADDRESS_ZERO: PreconferAddress = [0u8; 20];
+
+pub type Epoch = u64;
 
 // TODO for future usage
 // pub type BLSUncompressedPublicKey = [u8; 96];
