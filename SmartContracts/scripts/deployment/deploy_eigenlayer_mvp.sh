febfffd925f3ccdc9677a5bebb905f4c61bc8111
Jeffery Walsh <cyberhorsey@gmail.com>
2024-09-27 11:35:26 -0700

    updates for post-ontake fork changes, latest ABI, multi block support for the TaskManager

--- SmartContracts/scripts/deployment/deploy_eigenlayer_mvp.sh
+++ SmartContracts/scripts/deployment/deploy_eigenlayer_mvp.sh
@@ -1,7 +1,7 @@
 set -e
 
 export PRIVATE_KEY=0xbcdf20249abf0ed6d944c0288fad489e33f66b3960d9e6229c1cd214ed3bbe31
-export FORK_URL=
+export FORK_URL=http://127.0.0.1:8545
 
 forge script scripts/deployment/DeployEigenlayerMVP.s.sol:DeployEigenlayerMVP \
   --rpc-url $FORK_URL \
--- SmartContracts/src/interfaces/taiko/ITaikoL1.sol
+++ SmartContracts/src/interfaces/taiko/ITaikoL1.sol
@@ -2,23 +2,37 @@
 pragma solidity 0.8.25;
 
 interface ITaikoL1 {
-    struct BlockMetadata {
-        bytes32 l1Hash;
+    struct BaseFeeConfig {
+        uint8 adjustmentQuotient;
+        uint8 sharingPctg;
+        uint32 gasIssuancePerSecond;
+        uint64 minGasExcess;
+        uint32 maxGasIssuancePerBlock;
+    }
+
+    struct BlockMetadataV2 {
+        bytes32 anchorBlockHash; // `_l1BlockHash` in TaikoL2's anchor tx.
         bytes32 difficulty;
         bytes32 blobHash;
         bytes32 extraData;
-        bytes32 depositsHash;
         address coinbase;
         uint64 id;
         uint32 gasLimit;
         uint64 timestamp;
-        uint64 l1Height;
+        uint64 anchorBlockId; // `_l1BlockId` in TaikoL2's anchor tx.
         uint16 minTier;
         bool blobUsed;
         bytes32 parentMetaHash;
-        address sender;
+        address proposer;
+        uint96 livenessBond;
+        // Time this block is proposed at, used to check proving window and cooldown window.
+        uint64 proposedAt;
+        // L1 block number, required/used by node/client.
+        uint64 proposedIn;
         uint32 blobTxListOffset;
         uint32 blobTxListLength;
+        uint8 blobIndex;
+        BaseFeeConfig baseFeeConfig;
     }
 
     struct EthDeposit {
@@ -44,25 +58,44 @@ interface ITaikoL1 {
         uint64 lastUnpausedAt;
     }
 
-    struct Block {
-        bytes32 metaHash;
-        address assignedProver;
+    struct BlockV2 {
+        bytes32 metaHash; // slot 1
+        address assignedProver; // slot 2
         uint96 livenessBond;
-        uint64 blockId;
+        uint64 blockId; // slot 3
+        // Before the fork, this field is the L1 timestamp when this block is proposed.
+        // After the fork, this is the timestamp of the L2 block.
+        // In a later fork, we an rename this field to `timestamp`.
         uint64 proposedAt;
+        // Before the fork, this field is the L1 block number where this block is proposed.
+        // After the fork, this is the L1 block number input for the anchor transaction.
+        // In a later fork, we an rename this field to `anchorBlockId`.
         uint64 proposedIn;
-        uint32 nextTransitionId;
-        uint32 verifiedTransitionId;
-        uint64 timestamp;
-        uint32 l1StateBlockNumber;
+        uint24 nextTransitionId;
+        bool livenessBondReturned;
+        // The ID of the transaction that is used to verify this block. However, if
+        // this block is not verified as the last block in a batch, verifiedTransitionId
+        // will remain zero.
+        uint24 verifiedTransitionId;
     }
 
-    function proposeBlock(bytes calldata _params, bytes calldata _txList)
+    function proposeBlockV2(
+        bytes calldata _params,
+        bytes calldata _txList
+    )
         external
         payable
-        returns (BlockMetadata memory meta_, EthDeposit[] memory deposits_);
+        returns (BlockMetadataV2 memory meta_);
+
+     function proposeBlocksV2(
+        bytes[] calldata _paramsArr,
+        bytes[] calldata _txListArr
+    )
+        external
+        payable
+        returns (BlockMetadataV2[] memory metaArr_);
 
     function getStateVariables() external view returns (SlotA memory, SlotB memory);
 
-    function getBlock(uint64 _blockId) external view returns (Block memory blk_);
+    function getBlockV2(uint64 _blockId) external view returns (BlockV2 memory blk_);
 }
