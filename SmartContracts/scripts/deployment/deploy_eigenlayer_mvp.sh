7aa7e6896712cac5d5cae193e79467953a09402d
nmjustinchan <justin.chan@nethermind.io>
2024-09-28 23:44:27 +1000

    refac: update avs-deploy Dockerfile, eigenlayer mvp and avs scripts for image build

--- SmartContracts/scripts/deployment/deploy_eigenlayer_mvp.sh
+++ SmartContracts/scripts/deployment/deploy_eigenlayer_mvp.sh
@@ -1,5 +1,8 @@
 set -e
 
+: "${PRIVATE_KEY:?Environment variable PRIVATE_KEY is required}"
+: "${FORK_URL:?Environment variable FORK_URL is required}"
+
 forge script scripts/deployment/DeployEigenlayerMVP.s.sol:DeployEigenlayerMVP \
   --rpc-url $FORK_URL \
   --broadcast \
