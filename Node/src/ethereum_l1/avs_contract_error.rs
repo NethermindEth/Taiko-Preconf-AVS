db0b5bbc544622c7eff50b9be9be2929970aa3cf
Maciej Skrzypkowski <mskr@gmx.com>
2024-10-01 10:41:04 +0200

    Improved handling of errors when payload is none

--- Node/src/ethereum_l1/avs_contract_error.rs
+++ Node/src/ethereum_l1/avs_contract_error.rs
@@ -7,47 +7,57 @@ pub trait AVSContractError {
 
 impl AVSContractError for Error {
     fn to_avs_contract_error(&self) -> String {
+        let convert_error_code_to_message = |error_code: u32| -> String {
+            match error_code {
+                // PreconfRegistry
+                0xc3be614d => "PreconferAlreadyRegistered(): The preconfer is already registered in the registry".to_string(),
+                0x3b6c5f24 => "PreconferNotRegistered(): The preconfer is not registered in the registry".to_string(),
+                0x62278171 => "InvalidValidatorSignature(): The signature is invalid".to_string(),
+                0x6c06063b => "ValidatorSignatureExpired(): The signature has expired".to_string(),
+                0x43f8f5e9 => "ValidatorAlreadyActive(): The validator is already proposing for a preconfer and cannot be added again without removal".to_string(),
+                0x2868d153 => "ValidatorAlreadyInactive(): The validator is already removed or waiting to stop proposing for a preconfer".to_string(),
+                // PreconfServiceManager
+                0x42faedfe => "SenderIsNotPreconfTaskManager(): Only callable by the task manager".to_string(),
+                0xcbb45a59 => "SenderIsNotPreconfRegistry(): Only callable by the registry".to_string(),
+                0x54357847 => "OperatorAlreadySlashed(): The operator is already slashed".to_string(),
+                // PreconfTaskManager
+                0xbe4e4f53 => "InvalidLookaheadPointer(): The current (or provided) timestamp does not fall in the range provided by the lookahead pointer".to_string(),
+                0x1502053a => "SenderIsNotThePreconfer(): The block proposer is not the assigned preconfer for the current slot/timestamp".to_string(),
+                0x971cce8f => "InvalidSlotTimestamp(): The timestamp in the lookahead is not of a valid future slot in the present epoch".to_string(),
+                0xd74dc18f => "PreconfirmationChainIdMismatch(): The chain id on which the preconfirmation was signed is different from the current chain's id".to_string(),
+                0x005b3ac2 => "MissedDisputeWindow(): The dispute window for proving incorrect lookahead or preconfirmation is over".to_string(),
+                0x7855bfd4 => "PreconfirmationIsCorrect(): The disputed preconfirmation is correct".to_string(),
+                0xc0abeb9f => "MetadataMismatch(): The sent block metadata does not match the one retrieved from Taiko".to_string(),
+                0xf23592f9 => "PosterAlreadySlashedOrLookaheadIsEmpty(): The lookahead poster for the epoch has already been slashed or there is no lookahead for epoch".to_string(),
+                0xdd52015e => "LookaheadEntryIsCorrect(): The lookahead preconfer matches the one the actual validator is proposing for".to_string(),
+                0xc08f610e => "LookaheadIsNotRequired(): Cannot force push a lookahead since it is not lagging behind".to_string(),
+                // Taiko L1
+                0x3a0e4c1a => "L1_FORK_ERROR(): TaikoL1 error.".to_string(),
+                0x36c7c689 => "L1_INVALID_PARAMS(): TaikoL1 error.".to_string(),
+                0xdf9969ef => "L1_BLOB_NOT_AVAILABLE(): TaikoL1 error.".to_string(),
+                0x9e7e2ddd => "L1_BLOB_NOT_FOUND(): TaikoL1 error.".to_string(),
+                0x618e4902 => "L1_INVALID_ANCHOR_BLOCK(): TaikoL1 error.".to_string(),
+                0xc043062a => "L1_INVALID_CUSTOM_PROPOSER(): TaikoL1 error.".to_string(),
+                0xd6e2c5a0 => "L1_INVALID_PROPOSER(): TaikoL1 error.".to_string(),
+                0x13f7f80d => "L1_INVALID_TIMESTAMP(): TaikoL1 error.".to_string(),
+                0x37e6fc42 => "L1_LIVENESS_BOND_NOT_RECEIVED(): TaikoL1 error.".to_string(),
+                0x51ec7d53 => "L1_TOO_MANY_BLOCKS(): TaikoL1 error.".to_string(),
+                0x1a83d90e => "L1_UNEXPECTED_PARENT(): TaikoL1 error.".to_string(),
+                _ => format!("Unknown error {:#?}", self),
+            }
+        };
+
         if let Error::TransportError(TransportError::ErrorResp(e)) = self {
             if let Some(data) = e.data.as_ref().map(|d| d.get()) {
                 if let Ok(error_code) = u32::from_str_radix(&data[3..data.len() - 1], 16) {
-                    return match error_code {
-                        // PreconfRegistry
-                        0xc3be614d => "PreconferAlreadyRegistered(): The preconfer is already registered in the registry".to_string(),
-                        0x3b6c5f24 => "PreconferNotRegistered(): The preconfer is not registered in the registry".to_string(),
-                        0x62278171 => "InvalidValidatorSignature(): The signature is invalid".to_string(),
-                        0x6c06063b => "ValidatorSignatureExpired(): The signature has expired".to_string(),
-                        0x43f8f5e9 => "ValidatorAlreadyActive(): The validator is already proposing for a preconfer and cannot be added again without removal".to_string(),
-                        0x2868d153 => "ValidatorAlreadyInactive(): The validator is already removed or waiting to stop proposing for a preconfer".to_string(),
-                        // PreconfServiceManager
-                        0x42faedfe => "SenderIsNotPreconfTaskManager(): Only callable by the task manager".to_string(),
-                        0xcbb45a59 => "SenderIsNotPreconfRegistry(): Only callable by the registry".to_string(),
-                        0x54357847 => "OperatorAlreadySlashed(): The operator is already slashed".to_string(),
-                        // PreconfTaskManager
-                        0xbe4e4f53 => "InvalidLookaheadPointer(): The current (or provided) timestamp does not fall in the range provided by the lookahead pointer".to_string(),
-                        0x1502053a => "SenderIsNotThePreconfer(): The block proposer is not the assigned preconfer for the current slot/timestamp".to_string(),
-                        //0x3b6c5f24 => "PreconferNotRegistered(): Preconfer is not present in the registry".to_string(),
-                        0x971cce8f => "InvalidSlotTimestamp(): The timestamp in the lookahead is not of a valid future slot in the present epoch".to_string(),
-                        0xd74dc18f => "PreconfirmationChainIdMismatch(): The chain id on which the preconfirmation was signed is different from the current chain's id".to_string(),
-                        0x005b3ac2 => "MissedDisputeWindow(): The dispute window for proving incorrect lookahead or preconfirmation is over".to_string(),
-                        0x7855bfd4 => "PreconfirmationIsCorrect(): The disputed preconfirmation is correct".to_string(),
-                        0xc0abeb9f => "MetadataMismatch(): The sent block metadata does not match the one retrieved from Taiko".to_string(),
-                        0xf23592f9 => "PosterAlreadySlashedOrLookaheadIsEmpty(): The lookahead poster for the epoch has already been slashed or there is no lookahead for epoch".to_string(),
-                        0xdd52015e => "LookaheadEntryIsCorrect(): The lookahead preconfer matches the one the actual validator is proposing for".to_string(),
-                        0xc08f610e => "LookaheadIsNotRequired(): Cannot force push a lookahead since it is not lagging behind".to_string(),
-                        // Taiko L1
-                        0x3a0e4c1a => "L1_FORK_ERROR(): TaikoL1 error.".to_string(),
-                        0x36c7c689 => "L1_INVALID_PARAMS(): TaikoL1 error.".to_string(),
-                        0xdf9969ef => "L1_BLOB_NOT_AVAILABLE(): TaikoL1 error.".to_string(),
-                        0x9e7e2ddd => "L1_BLOB_NOT_FOUND(): TaikoL1 error.".to_string(),
-                        0x618e4902 => "L1_INVALID_ANCHOR_BLOCK(): TaikoL1 error.".to_string(),
-                        0xc043062a => "L1_INVALID_CUSTOM_PROPOSER(): TaikoL1 error.".to_string(),
-                        0xd6e2c5a0 => "L1_INVALID_PROPOSER(): TaikoL1 error.".to_string(),
-                        0x13f7f80d => "L1_INVALID_TIMESTAMP(): TaikoL1 error.".to_string(),
-                        0x37e6fc42 => "L1_LIVENESS_BOND_NOT_RECEIVED(): TaikoL1 error.".to_string(),
-                        0x51ec7d53 => "L1_TOO_MANY_BLOCKS(): TaikoL1 error.".to_string(),
-                        0x1a83d90e => "L1_UNEXPECTED_PARENT(): TaikoL1 error.".to_string(),
-                        _ => format!("Unknown error {:#?}", self),
-                    };
+                    return convert_error_code_to_message(error_code);
+                }
+            } else {
+                if e.message.starts_with("Reverted ") && e.message.len() >= 13 {
+                    let code = &e.message[9..13];
+                    let error_code = code.as_bytes();
+                    let error_code = u32::from_be_bytes(error_code.try_into().unwrap_or([0; 4]));
+                    return convert_error_code_to_message(error_code);
                 }
             }
         }
@@ -153,5 +163,35 @@ mod tests {
         } else {
             assert!(false);
         }
+    }
+
+    use alloy_json_rpc::ErrorPayload;
+
+    #[test]
+    fn test_transport_error_registered() {
+        let e = TransportError::ErrorResp(ErrorPayload {
+            code: 429,
+            message: "Reverted þaM".to_string(),
+            data: None,
+        });
+
+        let err = Error::TransportError(e);
+        assert!(err
+            .to_avs_contract_error()
+            .starts_with("PreconferAlreadyRegistered"));
+    }
+
+    #[test]
+    fn test_transport_error_registered_add_validator() {
+        let e = TransportError::ErrorResp(ErrorPayload {
+            code: 429,
+            message: "Reverted ;l_$".to_string(),
+            data: None,
+        });
+
+        let err = Error::TransportError(e);
+        assert!(err
+            .to_avs_contract_error()
+            .starts_with("PreconferNotRegistered"));
     }
 }
