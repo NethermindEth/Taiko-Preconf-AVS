1923256b2425d2ca83746c7ac5e29c18925952e8
Daniel Wang <dan@taiko.xyz>
2024-10-02 16:16:58 +0800

    fix

--- SmartContracts/test/libraries/MerkleUtils.t.sol
+++ SmartContracts/test/libraries/MerkleUtils.t.sol
@@ -6,51 +6,68 @@ import {BaseTest} from "../BaseTest.sol";
 
 contract TestMerkleUtils is BaseTest {
     using MerkleUtils for bytes32[8];
+    using MerkleUtils for bytes32;
 
     function test_merkleize_and_verify_chunks() external pure {
         bytes32[8] memory chunks;
 
-        chunks[0] = keccak256("chunk0");
-        chunks[1] = keccak256("chunk1");
-        chunks[2] = keccak256("chunk2");
-        chunks[3] = keccak256("chunk3");
-        chunks[4] = keccak256("chunk4");
-        chunks[5] = keccak256("chunk5");
-        chunks[6] = keccak256("chunk6");
-        chunks[7] = keccak256("chunk7");
+        chunks[0] = sha256("chunk0");
+        chunks[1] = sha256("chunk1");
+        chunks[2] = sha256("chunk2");
+        chunks[3] = sha256("chunk3");
+        chunks[4] = sha256("chunk4");
+        chunks[5] = sha256("chunk5");
+        chunks[6] = sha256("chunk6");
+        chunks[7] = sha256("chunk7");
 
-        bytes32 chunk0 = keccak256(abi.encodePacked(chunks[0], chunks[0]));
-        // bytes32 chunk0 = chunks[0];
-        bytes32 expectedRoot = keccak256(
+        bytes32 expectedRoot = sha256(
             abi.encodePacked(
-                keccak256(
-                    abi.encodePacked(
-                        keccak256(abi.encodePacked(chunk0, chunks[1])), //
-                        keccak256(abi.encodePacked(chunks[2], chunks[3]))
-                    )
-                ),
-                keccak256(
-                    abi.encodePacked(
-                        keccak256(abi.encodePacked(chunks[4], chunks[5])),
-                        keccak256(abi.encodePacked(chunks[6], chunks[7]))
-                    )
-                )
+                sha256(abi.encodePacked(chunks[0].hash(chunks[1]), chunks[2].hash(chunks[3]))),
+                sha256(abi.encodePacked(chunks[4].hash(chunks[5]), chunks[6].hash(chunks[7])))
             )
         );
-        // [FAIL: assertion failed: 0xf386e34e7fc1bf9aa3178012232d02f781a6ddf5b2e08c330a5f457557e85627 != 0xc61f6863f580527844f8529bfe593ad9c319c641d385b6413a341ff4383032dd]
         assertEq(chunks.merkleize(), expectedRoot);
 
-        // bytes32[] memory proof = new bytes32[](3);
-        // proof[0] = keccak256("chunk2");
-        // proof[1] = keccak256(abi.encodePacked(keccak256("chunk3"), keccak256("chunk4")));
-        // proof[2] = keccak256(
-        //     abi.encodePacked(
-        //         keccak256(abi.encodePacked(keccak256("chunk5"), keccak256("chunk6"))),
-        //         keccak256(abi.encodePacked(keccak256("chunk7"), keccak256("chunk8")))
-        //     )
-        // );
+        bytes32[] memory proof = new bytes32[](3);
+        proof[0] = chunks[1];
+        proof[1] = chunks[2].hash(chunks[3]);
+        proof[2] = sha256(abi.encodePacked(chunks[4].hash(chunks[5]), chunks[6].hash(chunks[7])));
 
-        // bool result = MerkleUtils.verifyProof(proof, root, keccak256("chunk1"), 0);
-        // assertTrue(result, "The proof should be valid");
+        assertTrue(MerkleUtils.verifyProof(proof, expectedRoot, chunks[0], 0));
+
+        proof[0] = chunks[0];
+        proof[1] = chunks[2].hash(chunks[3]);
+        proof[2] = sha256(abi.encodePacked(chunks[4].hash(chunks[5]), chunks[6].hash(chunks[7])));
+        assertTrue(MerkleUtils.verifyProof(proof, expectedRoot, chunks[1], 1));
+
+        proof[0] = chunks[3];
+        proof[1] = chunks[0].hash(chunks[1]);
+        proof[2] = sha256(abi.encodePacked(chunks[4].hash(chunks[5]), chunks[6].hash(chunks[7])));
+        assertTrue(MerkleUtils.verifyProof(proof, expectedRoot, chunks[2], 2));
+
+        proof[0] = chunks[2];
+        proof[1] = chunks[0].hash(chunks[1]);
+        proof[2] = sha256(abi.encodePacked(chunks[4].hash(chunks[5]), chunks[6].hash(chunks[7])));
+        assertTrue(MerkleUtils.verifyProof(proof, expectedRoot, chunks[3], 3));
+
+        proof[0] = chunks[5];
+        proof[1] = chunks[6].hash(chunks[7]);
+        proof[2] = sha256(abi.encodePacked(chunks[0].hash(chunks[1]), chunks[2].hash(chunks[3])));
+        assertTrue(MerkleUtils.verifyProof(proof, expectedRoot, chunks[4], 4));
+
+        proof[0] = chunks[4];
+        proof[1] = chunks[6].hash(chunks[7]);
+        proof[2] = sha256(abi.encodePacked(chunks[0].hash(chunks[1]), chunks[2].hash(chunks[3])));
+        assertTrue(MerkleUtils.verifyProof(proof, expectedRoot, chunks[5], 5));
+
+        proof[0] = chunks[7];
+        proof[1] = chunks[4].hash(chunks[5]);
+        proof[2] = sha256(abi.encodePacked(chunks[0].hash(chunks[1]), chunks[2].hash(chunks[3])));
+        assertTrue(MerkleUtils.verifyProof(proof, expectedRoot, chunks[6], 6));
+
+        proof[0] = chunks[6];
+        proof[1] = chunks[4].hash(chunks[5]);
+        proof[2] = sha256(abi.encodePacked(chunks[0].hash(chunks[1]), chunks[2].hash(chunks[3])));
+        assertTrue(MerkleUtils.verifyProof(proof, expectedRoot, chunks[7], 7));
     }
 }
