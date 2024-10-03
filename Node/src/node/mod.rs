a73b5f41ff632b3e71dd24a8763c196997f9eb7f
Maciej Skrzypkowski <mskr@gmx.com>
2024-10-03 11:14:21 +0200

    increasing the block id for block preconfirmation

--- Node/src/node/mod.rs
+++ Node/src/node/mod.rs
@@ -315,10 +315,6 @@ impl Node {
                 self.preconfirm_last_slot().await?;
             }
             OperatorStatus::Preconfer => {
-                if !self.is_preconfer_now.load(Ordering::Acquire) {
-                    self.is_preconfer_now.store(true, Ordering::Release);
-                    self.start_propose().await?;
-                }
                 self.preconfirm_block(true).await?;
             }
             OperatorStatus::None => {
@@ -440,6 +436,11 @@ impl Node {
                 .get_l2_slot_number_within_l1_slot()?
         );
 
+        if !self.is_preconfer_now.load(Ordering::Acquire) {
+            self.is_preconfer_now.store(true, Ordering::Release);
+            self.start_propose().await?;
+        }
+
         let lookahead_params = self.get_lookahead_params().await?;
         let pending_tx_lists = self.taiko.get_pending_l2_tx_lists().await?;
         let pending_tx_lists_bytes = if pending_tx_lists.tx_list_bytes.is_empty() {
@@ -469,7 +470,10 @@ impl Node {
             pending_tx_lists.tx_list_bytes[0].clone() // TODO: handle multiple tx lists
         };
 
-        let new_block_height = pending_tx_lists.parent_block_id + 1;
+        let new_block_height = self
+            .preconfirmation_helper
+            .get_new_block_id(pending_tx_lists.parent_block_id);
+        debug!("Preconfirming block with the height: {}", new_block_height);
 
         let (commit_hash, signature) =
             self.generate_commit_hash_and_signature(&pending_tx_lists, new_block_height)?;
--- Node/src/node/preconfirmation_helper.rs
+++ Node/src/node/preconfirmation_helper.rs
@@ -1,10 +1,14 @@
 pub struct PreconfirmationHelper {
     nonce: u64,
+    last_block_id: u64,
 }
 
 impl PreconfirmationHelper {
     pub fn new() -> Self {
-        Self { nonce: 0 }
+        Self {
+            nonce: 0,
+            last_block_id: 0,
+        }
     }
 
     pub fn init(&mut self, nonce: u64) {
@@ -19,5 +23,33 @@ impl PreconfirmationHelper {
 
     pub fn increment_nonce(&mut self) {
         self.nonce += 1;
+    }
+
+    pub fn get_new_block_id(&mut self, parent_block_id: u64) -> u64 {
+        let mut new_block_id = parent_block_id + 1;
+        if self.last_block_id >= new_block_id {
+            new_block_id = self.last_block_id + 1;
+        }
+        self.last_block_id = new_block_id;
+        new_block_id
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_new_block_id() {
+        let mut helper = PreconfirmationHelper::new();
+        assert_eq!(helper.get_new_block_id(0), 1);
+        assert_eq!(helper.get_new_block_id(0), 2);
+        assert_eq!(helper.get_new_block_id(0), 3);
+        assert_eq!(helper.get_new_block_id(0), 4);
+        assert_eq!(helper.get_new_block_id(4), 5);
+        assert_eq!(helper.get_new_block_id(4), 6);
+        assert_eq!(helper.get_new_block_id(4), 7);
+        assert_eq!(helper.get_new_block_id(4), 8);
     }
 }
