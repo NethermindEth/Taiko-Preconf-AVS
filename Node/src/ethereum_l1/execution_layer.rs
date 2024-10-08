f58b59933bccd37724015527172272d9bef31367
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2024-10-08 09:47:02 +0200

    Bump version to 0.1.3

--- Node/src/ethereum_l1/execution_layer.rs
+++ Node/src/ethereum_l1/execution_layer.rs
@@ -276,7 +276,7 @@ impl ExecutionLayer {
         if send_to_contract {
             let pending = self.provider_ws.send_raw_transaction(&buf).await?;
 
-            tracing::debug!("Send raw transaction, with hash {}", pending.tx_hash());
+            tracing::debug!("Sending raw transaction, with hash {}", pending.tx_hash());
         }
 
         Ok(buf)
--- Node/src/node/mod.rs
+++ Node/src/node/mod.rs
@@ -20,7 +20,7 @@ use operator::{Operator, Status as OperatorStatus};
 use preconfirmation_helper::PreconfirmationHelper;
 use preconfirmation_message::PreconfirmationMessage;
 use preconfirmation_proof::PreconfirmationProof;
-use tracing_subscriber::field::debug;
+
 use std::{
     collections::HashMap,
     sync::{
@@ -517,7 +517,10 @@ impl Node {
             )
             .await?;
 
-        debug!("Proposed new block, with hash {}", alloy::primitives::keccak256(&tx));
+        debug!(
+            "Proposed new block, with hash {}",
+            alloy::primitives::keccak256(&tx)
+        );
         // insert transaction
         self.preconfirmation_txs
             .lock()
