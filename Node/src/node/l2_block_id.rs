940708a8d14e6bd1ac2dfeaedb8b40d7c7715943
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2024-10-09 10:59:03 +0200

    Refactor: improve L2BlockId implementation

--- Node/src/node/l2_block_id.rs
+++ Node/src/node/l2_block_id.rs
@@ -11,40 +11,38 @@ impl L2BlockId {
         }
     }
 
+    // Update self.block_id with the maximum value between self.block_id and block_id
     pub fn update(&self, block_id: u64) {
-        let mut current_block_id = self.block_id.load(Ordering::Acquire);
-
-        while block_id > current_block_id {
-            match self.block_id.compare_exchange(
-                current_block_id,
-                block_id,
-                Ordering::Release,
-                Ordering::Acquire,
-            ) {
-                Ok(_) => break,
-                Err(previous) => current_block_id = previous,
-            }
-        }
+        self.block_id.fetch_max(block_id, Ordering::AcqRel);
     }
 
+    // Returns the next block ID
+    // The next block ID is computed as the maximum value between current_block_id + 1 and new_block_id + 1
     pub fn next(&self, block_id: u64) -> u64 {
+        // Get the current value of current_block_id
         let mut current_block_id = self.block_id.load(Ordering::Acquire);
-        let mut new_block_id = std::cmp::max(block_id + 1, current_block_id + 1);
-        while new_block_id > current_block_id {
+        // Initialize new_block_id
+        let mut new_block_id = block_id + 1;
+    
+        loop {
+            // Get next block ID 
+            // It is the maximum value between current_block_id + 1 and new_block_id + 1
+            new_block_id = new_block_id.max(current_block_id + 1);
+
+            // Attempt to update the block ID using a compare-exchange operation
             match self.block_id.compare_exchange(
                 current_block_id,
                 new_block_id,
                 Ordering::Release,
                 Ordering::Acquire,
             ) {
-                Ok(_) => break,
+                Ok(_) => return new_block_id, // Return immediately on success
                 Err(previous) => {
                     current_block_id = previous;
-                    new_block_id = std::cmp::max(new_block_id, current_block_id + 1);
+                    // new_block_id gets recalculated at the start of the loop
                 }
             }
         }
-        new_block_id
     }
 }
 
@@ -75,4 +73,19 @@ mod tests {
         l2_block_id.update(5);
         assert_eq!(l2_block_id.block_id.load(Ordering::SeqCst), 10);
     }
+
+    #[test]
+    fn test_update_next() {
+        let l2_block_id = L2BlockId::new();
+        
+        l2_block_id.update(1);
+        assert_eq!(l2_block_id.block_id.load(Ordering::SeqCst), 1);
+        l2_block_id.update(10);
+        assert_eq!(l2_block_id.block_id.load(Ordering::SeqCst), 10);
+        assert_eq!(l2_block_id.next(0), 11);
+        assert_eq!(l2_block_id.next(12), 13);
+        l2_block_id.update(5);
+        assert_eq!(l2_block_id.block_id.load(Ordering::SeqCst), 13);
+    }
+
 }
