8e5e5b13a5e2f6ad15b84e3b381a04d956dddc53
Maciej Skrzypkowski <mskr@gmx.com>
2025-01-16 13:00:07 +0100

    not needed test

--- Node/src/ethereum_l1/execution_layer.rs
+++ Node/src/ethereum_l1/execution_layer.rs
@@ -1100,97 +1100,25 @@ mod tests {
         let rpc_url: reqwest::Url = anvil.endpoint().parse().unwrap();
         let ws_rpc_url = anvil.ws_endpoint();
         let private_key = anvil.keys()[0].clone();
-        let el = ExecutionLayer::new_from_pk(ws_rpc_url, rpc_url, private_key)
+        let _el = ExecutionLayer::new_from_pk(ws_rpc_url, rpc_url, private_key)
             .await
             .unwrap();
 
-        let epoch_begin_timestamp = 0;
-        let validator_bls_pub_keys: [BLSCompressedPublicKey; 32] = [[0u8; 48]; 32];
+        let _epoch_begin_timestamp = 0;
+        let _validator_bls_pub_keys: [BLSCompressedPublicKey; 32] = [[0u8; 48]; 32];
 
-        let contract = PreconfTaskManager::new(
-            el.contract_addresses.avs.preconf_task_manager,
-            &el.provider_ws,
-        );
         // TODO:
         // There is a bug in the Anvil (anvil 0.2.0) library:
         // `Result::unwrap()` on an `Err` value: buffer overrun while deserializing
         // check if it's fixed in next version
-        let lookahead_params = el
-            .get_lookahead_params_for_epoch(epoch_begin_timestamp, &validator_bls_pub_keys)
-            .await
-            .unwrap();
+        // let lookahead_params = el
+        //     .get_lookahead_params_for_epoch(epoch_begin_timestamp, &validator_bls_pub_keys)
+        //     .await
+        //     .unwrap();
         // assert!(
         //     !lookahead_params.is_empty(),
         //     "Lookahead params should not be empty"
         // );
-    }
-
-    use alloy::json_abi::JsonAbi;
-
-    #[tokio::test]
-    async fn test_decode_preconf_task_manager_abi() {
-        let abi = include_str!("abi/PreconfTaskManager.json");
-
-        // let path = std::env::current_dir()?.join("examples/advanced/examples/abi/SimpleLending.json");
-        // let contents = std::fs::read(path)?;
-        let abi: JsonAbi = serde_json::from_str(abi).unwrap();
-
-        // Print deserialized ABI components
-        println!("Deserialized ABI:");
-
-        // Constructor
-        if let Some(constructor) = &abi.constructor {
-            println!("\n>> Constructor:");
-            println!("  Inputs: {:?}", constructor.inputs);
-            println!("  State mutability: {:?}", constructor.state_mutability);
-        }
-
-        println!("\n=========\n");
-
-        // Functions
-        println!("Functions:");
-        for (name, functions) in &abi.functions {
-            println!("\n>> {}:", name);
-            for function in functions {
-                println!("    Inputs: {:?}", function.inputs);
-                println!("    Outputs: {:?}", function.outputs);
-                println!("    State mutability: {:?}", function.state_mutability);
-            }
-        }
-
-        println!("\n=========\n");
-
-        // Events
-        println!("Events:");
-        for (name, events) in &abi.events {
-            println!("\n>> {}:", name);
-            for event in events {
-                println!("    Inputs: {:?}", event.inputs);
-                println!("    Anonymous: {}", event.anonymous);
-            }
-        }
-
-        println!("\n=========\n");
-
-        // Errors
-        println!("Errors:");
-        for (name, errors) in &abi.errors {
-            println!(">> {}:", name);
-            for error in errors {
-                println!("    Inputs: {:?}", error.inputs);
-            }
-        }
-
-        println!("\n=========\n");
-
-        // Example of working with a specific function
-        if let Some(add_collateral) = abi.functions.get("addCollateral").and_then(|f| f.first()) {
-            println!("Example: addCollateral() function exists!");
-            println!("Inputs:");
-            for input in &add_collateral.inputs {
-                println!("  Name: {}, Type: {}", input.name, input.ty);
-            }
-        }
     }
 
     #[tokio::test]
