f711aa399a9e7b5eae7d40c6c22bd44cc7b686f0
Lin Oshitani <linoshitani@gmail.com>
2025-03-13 17:08:23 +0900

    Improve error handling
    Co-authored-by: Maciej Skrzypkowski <mskr@gmx.com>

--- Node/src/ethereum_l1/execution_layer.rs
+++ Node/src/ethereum_l1/execution_layer.rs
@@ -166,7 +166,10 @@ impl ExecutionLayer {
 
         // TODO estimate gas and select blob or calldata transaction
 
-        let last_block_timestamp = l2_blocks.last().unwrap().timestamp_sec;
+        let last_block_timestamp = l2_blocks
+            .last()
+            .ok_or(anyhow::anyhow!("No L2 blocks provided"))?
+            .timestamp_sec;
         let hash = self
             .propose_batch_blob(
                 nonce,
