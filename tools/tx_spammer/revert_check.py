95c6ea9e3ad72d0057eccf18e6bb5f32abebaf24
Maciej Skrzypkowski <mskr@gmx.com>
2025-04-01 11:00:27 +0200

    Web sockets for revert check script

--- tools/tx_spammer/revert_check.py
+++ tools/tx_spammer/revert_check.py
@@ -6,13 +6,16 @@ def main():
     parser.add_argument('--tx-hash', type=str, required=True,
                         help='Transaction hash to check (e.g., 0x1234...)')
     parser.add_argument('--rpc-url', type=str, required=True,
-                        help='HTTP endpoint of the Ethereum node')
+                        help='HTTP or WebSocket endpoint of the Ethereum node')
 
     args = parser.parse_args()
     tx_hash = args.tx_hash
     rpc_url = args.rpc_url
 
-    w3 = Web3(Web3.HTTPProvider(rpc_url))
+    if rpc_url.startswith('ws://') or rpc_url.startswith('wss://'):
+        w3 = Web3(Web3.LegacyWebSocketProvider(rpc_url))
+    else:
+        w3 = Web3(Web3.HTTPProvider(rpc_url))
 
     # Get the transaction and its receipt
     tx = w3.eth.get_transaction(tx_hash)
