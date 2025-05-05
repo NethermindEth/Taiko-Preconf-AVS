1882ea0e60a6c63f69595bd0588ff0962fa93826
Maciej Skrzypkowski <mskr@gmx.com>
2025-05-05 16:37:12 +0200

    corrected serde

--- Node/src/taiko/preconf_blocks.rs
+++ Node/src/taiko/preconf_blocks.rs
@@ -29,8 +29,9 @@ pub struct RemovePreconfBlockRequestBody {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct TaikoStatus {
+    #[serde(rename = "highestUnsafeL2PayloadBlockID")]
     pub highest_unsafe_l2_payload_block_id: u64,
+    #[serde(rename = "endOfSequencingMarkerReceived")]
     pub end_of_sequencing_marker_received: bool,
 }
