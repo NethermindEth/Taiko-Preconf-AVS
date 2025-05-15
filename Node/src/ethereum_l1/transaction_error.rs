659f8927545fad127daaa66b74ba6971c9495246
Maciej Skrzypkowski <mskr@gmx.com>
2025-05-15 11:28:03 +0200

    Reorg when transaction estimation fails (#408)
    * Reapply "Reorg when transaction estimation fails"
    
    This reverts commit 5588684148cd178ed4c2b26f015a4188e296d38a.
    
    * calldata estimation error handling
    
    * log
    
    * 0.2.60
    
    * clearing batches to send on transaction error

--- Node/src/ethereum_l1/transaction_error.rs
+++ Node/src/ethereum_l1/transaction_error.rs
@@ -1,7 +1,14 @@
 #[derive(Debug)]
 pub enum TransactionError {
+    EstimationFailed,
     TransactionReverted,
     NotConfirmed,
     UnsupportedTransactionType,
     GetBlockNumberFailed,
+}
+
+impl std::fmt::Display for TransactionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
