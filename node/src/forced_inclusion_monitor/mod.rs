5998932966f1db8708a9349ab8b5dae3163c78ae
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2025-06-11 21:18:28 +0200

    chore: move ForcedInclusionData to a separate file

--- node/src/forced_inclusion_monitor/mod.rs
+++ node/src/forced_inclusion_monitor/mod.rs
@@ -2,7 +2,6 @@ use alloy::primitives::Address;
 use alloy::rpc::types::Transaction;
 use alloy::sol_types::SolEvent;
 use anyhow::{Error, anyhow};
-use tokio::task::JoinHandle;
 
 use std::collections::VecDeque;
 use std::time::Duration;
@@ -17,89 +16,13 @@ use crate::ethereum_l1::l1_contracts_bindings::forced_inclusion_store::IForcedIn
     ForcedInclusion, ForcedInclusionConsumed, ForcedInclusionStored,
 };
 
-use crate::node::blob_parser::extract_transactions_from_blob;
+mod forced_inclusion_data;
+use forced_inclusion_data::ForcedInclusionData;
+
 use crate::utils::event_listener::listen_for_event;
 
 const MESSAGE_QUEUE_SIZE: usize = 20;
 const SLEEP_DURATION: Duration = Duration::from_secs(15);
-
-struct ForcedInclusionData {
-    index: usize,
-    txs_list: Option<Vec<Transaction>>,
-    blob_decoding_handle: Option<JoinHandle<()>>,
-    blob_decoding_token: Option<CancellationToken>,
-}
-
-impl ForcedInclusionData {
-    fn is_decoding_in_progress(&self) -> bool {
-        self.blob_decoding_handle
-            .as_ref()
-            .is_some_and(|h| !h.is_finished())
-    }
-
-    fn is_data_ready(&self) -> bool {
-        self.txs_list.is_some()
-    }
-
-    async fn cancel_current_task(&mut self) {
-        if let Some(blob_decoding_handle) = self.blob_decoding_handle.take() {
-            if let Some(blob_decoding_token) = self.blob_decoding_token.take() {
-                debug!("Cancelling blob decoding task for index {}", self.index);
-                blob_decoding_token.cancel();
-                while !blob_decoding_handle.is_finished() {
-                    std::thread::sleep(std::time::Duration::from_millis(10));
-                }
-                self.blob_decoding_handle = None;
-                self.blob_decoding_token = None;
-                debug!("Blob decoding task cancelled");
-            }
-        }
-    }
-
-    async fn reset(&mut self) {
-        self.cancel_current_task().await;
-        self.index = 0;
-        self.txs_list = None;
-    }
-
-    fn decode(
-        &mut self,
-        forced_inclusion: ForcedInclusion,
-        ethereum_l1: Arc<EthereumL1>,
-        next_forced_inclusion_data: Arc<Mutex<ForcedInclusionData>>,
-    ) {
-        let decoding_token = CancellationToken::new();
-        self.blob_decoding_token = Some(decoding_token.clone());
-
-        let handle = tokio::spawn(async move {
-            // Replace with your real decoding logic
-            tokio::select! {
-                _ = decoding_token.cancelled() => {
-                    info!("decoding task was cancelled.");
-                }
-                _ = async {
-                    info!("Decoding new ForcedInclusion...");
-                    let txs = match extract_transactions_from_blob(
-                        ethereum_l1,
-                        forced_inclusion.blobCreatedIn,
-                        [forced_inclusion.blobHash].to_vec(),
-                        forced_inclusion.blobByteOffset,
-                        forced_inclusion.blobByteSize
-                    ).await {
-                        Ok(txs) => Some(txs),
-                        Err(e) => {
-                            error!("Error decoding ForcedInclusion: {}", e);
-                            None
-                        }
-                    };
-                    next_forced_inclusion_data.lock().await.txs_list = txs;
-                    info!("Decoding complete.");
-                } => {}
-            }
-        });
-        self.blob_decoding_handle = Some(handle);
-    }
-}
 
 pub struct ForcedInclusionMonitor {
     ws_rpc_url: String,
