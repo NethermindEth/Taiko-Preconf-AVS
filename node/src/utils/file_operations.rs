ed5575443b15afd7ef27ab16fdea5820d1e93568
Maciej Skrzypkowski <mskr@gmx.com>
2025-07-10 09:11:20 +0200

    Better error logs for JWT secret reading

--- node/src/utils/file_operations.rs
+++ node/src/utils/file_operations.rs
@@ -5,9 +5,12 @@ pub fn read_jwt_secret(file_path: &str) -> Result<[u8; 32], Error> {
     let secret = std::fs::read_to_string(file_path)
         .map_err(|e| anyhow::anyhow!("Failed to read JWT secret from file: {}", e))?;
     let secret_bytes = hex::decode(secret.strip_prefix("0x").unwrap_or(&secret))
-        .map_err(|e| anyhow::anyhow!("Failed to decode hex string: {}", e))?;
-    let secret_bytes: [u8; 32] = secret_bytes
-        .try_into()
-        .map_err(|e| anyhow::anyhow!("Failed to convert secret bytes to [u8; 32]: {:?}", e))?;
+        .map_err(|e| anyhow::anyhow!(" Failed to decode hex string from JWT secret file: {}", e))?;
+    let secret_bytes: [u8; 32] = secret_bytes.try_into().map_err(|e| {
+        anyhow::anyhow!(
+            "Failed to convert secret bytes to [u8; 32] from JWT secret file: {:?}",
+            e
+        )
+    })?;
     Ok(secret_bytes)
 }
