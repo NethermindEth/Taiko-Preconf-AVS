78c7609233011ac6b5f6dffded260d7c6a8195f8
Maciej Skrzypkowski <mskr@gmx.com>
2025-07-15 14:11:35 +0200

    Handling of NotTheOperator and NotPreconfer errors (#547)
    * Handling of NotTheOperator and NotPreconfer errors
    
    * 0.2.106

--- node/src/ethereum_l1/transaction_error.rs
+++ node/src/ethereum_l1/transaction_error.rs
@@ -12,6 +12,7 @@ pub enum TransactionError {
     ReanchorRequired,
     BuildTransactionFailed,
     OldestForcedInclusionDue,
+    NotTheOperatorInCurrentEpoch,
 }
 
 impl std::fmt::Display for TransactionError {
