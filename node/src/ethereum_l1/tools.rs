b882410c59a536138b4436513cb9dd6941c9d3ce
lin <linoshitani@gmail.com>
2025-08-21 08:41:56 +0900

    cargo fmt

--- node/src/ethereum_l1/tools.rs
+++ node/src/ethereum_l1/tools.rs
@@ -32,7 +32,9 @@ pub fn check_oldest_forced_inclusion_due(err_str: &str) -> bool {
 pub fn check_for_not_the_operator_in_current_epoch(err_str: &str) -> bool {
     // TODO: for new contracts version we should remove NotTheOperator
     // as it was renamed to NotPreconfer
-    err_str.contains("0x47fac6c1") || err_str.contains("0x795e2f19") || err_str.contains("0xc0ec4b50")
+    err_str.contains("0x47fac6c1")
+        || err_str.contains("0x795e2f19")
+        || err_str.contains("0xc0ec4b50")
 }
 
 pub fn convert_error_payload(err: &str) -> Option<TransactionError> {
